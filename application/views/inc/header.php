4731bf3499873c88f00044cf32a245d366dfab9c
BohorquezBlanco <bohorquez.alexander.126@gmail.com>
2022-07-21 10:38:23 -1200

    Creacion de Header y Footer y cambio en routes/usuarios

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -9,8 +9,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="author" content="Jose Ramires">
     <meta name="generator" content="Hugo 0.88.1">
 	<title> Sistema Web</title>
-	<link rel="stylesheet" type="text/css" href= "<?php echo base_url();?>bootstrap/css/bootstrap.min.css"> 
-    <link rel="stylesheet" type="text/css" href= "<?php echo base_url();?>bootstrap/css/checkout.css"> 
-    <link rel="stylesheet" type="text/css" href= "<?php echo base_url();?>bootstrap/css/form-validation.css"> 
+	<link rel="stylesheet" type="text/css" href= "<?php echo base_url();?>micssjs/css/bootstrap.min.css"> 
+    <link rel="stylesheet" type="text/css" href= "<?php echo base_url();?>micssjs/css/checkout.css"> 
+    <link rel="stylesheet" type="text/css" href= "<?php echo base_url();?>micssjs/css/form-validation.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>micssjs/css/MiCss.css"> 
 </head>
 <body class="bg-light">
