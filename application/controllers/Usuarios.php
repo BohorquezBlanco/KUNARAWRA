ce4e2349faf1297a6aab32f98de6abdd7e672794
BohorquezBlanco <bohorquez.alexander.126@gmail.com>
2022-08-27 09:50:04 -1200

    Creacion de nuevo Inicio

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -5,24 +5,46 @@ class Usuarios extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('inc/header');
-		$this->load->view('inc/barrauno');
-		$this->load->view('inicio');
-		$this->load->view('inc/pie');
-		$this->load->view('inc/footer');
+		
+		$this->load->view('inicio/inicio');
+	
 	}
 	public function ingresar()
 	{
 		$this->load->view('inc/headerIngreso');
 		$this->load->view('inc/barrauno');
-		$this->load->view('ingreso');
+		$this->load->view('inicio/ingreso');
 		$this->load->view('inc/footer');
 	}
 	public function inscribirse()
 	{
 		$this->load->view('inc/headerInscribirse');
 		$this->load->view('inc/barrauno');
-		$this->load->view('inscripcion');
+		$this->load->view('inicio/inscripcion');
+		$this->load->view('inc/footer');
+	}
+	//InicioEstudiante
+	public function incioEstudiante()
+	{
+		$this->load->view('inc/headerInscribirse');
+		$this->load->view('inc/barrauno');
+		$this->load->view('estudiante/inicioEstudiante');
+		$this->load->view('inc/footer');
+	}
+	//InicioProfesor
+	public function inicioProfesor()
+	{
+		$this->load->view('inc/headerInscribirse');
+		$this->load->view('inc/barrauno');
+		$this->load->view('profesor/inicioProfesor');
+		$this->load->view('inc/footer');
+	}
+	//InicioAdministrador
+	public function inicioAdministrador()
+	{
+		$this->load->view('inc/headerInscribirse');
+		$this->load->view('inc/barrauno');
+		$this->load->view('administrador/inicioAdmi');
 		$this->load->view('inc/footer');
 	}
 	//probar la conexion 
